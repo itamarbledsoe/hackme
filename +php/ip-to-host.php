b5389d14c405b13ae739b2818c83c86ac381cd00
Itamar <itamar@diwip.com>
2016-06-13 19:46:04 +0300

    Added detecting user computer unique id via MAC in addition to local DNS

--- +php/ip-to-host.php
+++ +php/ip-to-host.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+	Make use of "arp -a"! This can help uniquely identify devices so as to keep logins from geting mixed up!
+*/
+
 if (isset($_GET['ip']))
 {
 	$ip = $_GET['ip'];
@@ -10,9 +14,45 @@ else
 	$ip = $_SERVER['REMOTE_ADDR'];
 	print "SERVER['REMOTE_ADDR']=$ip\r\n";
 }
-$res = shell_exec("nslookup $ip");
-// print $res."________________________\r\n";
-preg_match_all("/[^\s]*\.diwip\.com(?=[\r\n]*\w*\:\s*$ip)/is", $res, $arr);
-print implode("\r\n", $arr[0]);
+
+// Get Computer Names from IP
+if (isset($_GET['DNS']))
+{
+	$res = shell_exec("nslookup $ip");
+	print "\r\n________________________\r\nDNS:\r\n{$res}________________________\r\n";
+	
+	$reg = "/[^\s]*\.diwip\.\w+(?=[\r\n]*\w*\:\s*$ip)/i";
+	if (preg_match($reg, $res))
+	{
+		preg_match_all($reg, $res, $arr);
+		$final = $arr[0];
+	}
+	else
+	{
+		$final = array("Unknown");
+	}
+	
+	print implode("\r\n", $final);
+}
+
+// Get Computer MAC from IP
+if (isset($_GET['MAC']))
+{
+	$res = shell_exec("arp -a");
+	print "\r\n________________________\r\nMAC:\r\n{$res}________________________\r\n";
+	
+	$reg = "/$ip\s*([\w\d\-\:]{17})/i";
+	if (preg_match($reg, $res))
+	{
+		preg_match_all($reg, $res, $arr);
+		$final = $arr[1];
+	}
+	else
+	{
+		$final = array("Unknown");
+	}
+	
+	print implode("\r\n", $final);
+}
 
 ?>
